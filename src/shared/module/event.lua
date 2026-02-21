c0bc1fb0aa7ab013146f314e69d2c0d1f0da47c4
ThigasDevelopment <maia.thico@gmail.com>
2026-02-20 22:52:28 -0300

    ✨ feat(event): implement Event class with parameters and handler functionality

---
+++ src/shared/module/event.lua
@@ -0,0 +1,32 @@
+---@class EventParams
+---@field name string
+---@field attachedTo userdata
+---@field handler fun(...: any): any
+---@field remote? boolean
+
+---@class Event : EventParams
+---@field core Core
+class 'Event' {
+	---@param self Event
+	---@param core Core
+	---@param props EventParams
+	---@return Event
+	constructor = function (self, core, params)
+		self.core = core;
+
+		self.name = params.name;
+		self.attachedTo = params.attachedTo;
+		self.handler = params.handler;
+
+		self.remote = params.remote or false;
+		if (self.remote) then
+			addEvent (self.name, true);
+		end
+		addEventHandler (self.name, self.attachedTo, self.handler);
+
+		return self;
+	end,
+};
+
+---@type fun(core: Core, params: EventParams): Event
+Event = new 'Event';
